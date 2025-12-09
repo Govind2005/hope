6c02d35942d41a46a2178ac0c92a3e5f44dad69b
Govind2005 <realgovindkrishna@gmail.com>
2025-12-09 13:37:30 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-red-400">
+        hey
+        </div>
+    </>
+  )
+}
+
+export default App
